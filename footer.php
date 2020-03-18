2553bc617d174dcff6fd26a43cdce94377865de6
Marissa Culbreth <mculbreth1996@ufl.edu>
2020-03-18 18:00:38 -0400

    Update footer contents

--- footer.php
+++ footer.php
@@ -1,19 +1,25 @@
 <footer>
   <div class="row">
     <div class="col-md-4 d-flex align-items-center">
-      <nav>
-        <?php
+      <nav class="left-footer">
+        <h3 class="footer-h3">Stay Connected</h3>
+        <div class="col-lg-3">
+          <?php dynamic_sidebar('bottom-left-home-page'); ?>
+        </div>
+
+        <!-- <?php
           if(has_nav_menu('footer-left')){
             wp_nav_menu(array('theme_location' => 'footer-left' , 'container_class' => 'footer-left-class'));
           }else{
             echo "Please select a footer left menu through the dashboard";
           }
-        ?>
+        ?> -->
+
       </nav>
     </div>
 
     <div class="col-md-4 d-flex align-items-center">
-      <nav>
+      <nav class="middle-footer">
         <h3 class="footer-h3">Links</h3>
         <?php
           if(has_nav_menu('footer-middle')){
@@ -26,7 +32,7 @@
     </div>
 
     <div class="col-md-4 d-flex align-items-center">
-      <nav>
+      <nav class="right-footer">
         <h3 class="footer-h3">Navigate</h3>
         <?php
           if(has_nav_menu('footer-right')){
