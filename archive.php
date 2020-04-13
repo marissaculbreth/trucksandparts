3395a3c61d2b596390fdab1856fe1654388219ae
Marissa Culbreth <mculbreth1996@ufl.edu>
2020-04-13 15:48:17 -0400

    Update archive page

---
+++ archive.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+
+Template Name: Archive Page
+
+*/
+
+get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="twelve columns">
+      <h2><?php
+      if(is_category()){
+        single_cat_title();
+      }elseif(is_tag()){
+        single_tag_title();
+      }elseif(is_date()){
+        echo "Daily Archives: " . get_the_date();
+      }elseif(is_month()){
+        echo "Monthly Archives: " . get_the_date('F Y');
+      }elseif(is_year()){
+        echo "Yearly Archives: " . get_the_date('Y');
+      }else{
+        echo "Archives";
+      }
+      ?>
+      </h2>
+      <?php if(have_posts()){
+        while(have_posts()){
+          the_post();?>
+          <div class="twelve columns individual-post">
+
+            <div class="archive-page">
+              <div class="post-featured-image">
+                <?php the_post_thumbnail('thumbnail'); ?>
+              </div>
+              <div class="archive-info">
+                <h3 class="post-h3"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+                <p>Published on <?php the_time('F j, Y'); ?></p>
+                <p><?php the_excerpt(); ?></p>
+                <a class="btn btn-primary" href="<?php the_permalink() ?>">Read More</a>
+              </div>
+            </div>
+
+          </div>
+          <?php
+        } //ends while loop
+      } // ends if statement
+      ?>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -6,15 +6,24 @@
       while(have_posts()) {
         the_post(); ?>
         <div class="col-md-4 blog-main">
+
+          <h3 class="post-h3"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
           <div class="post-featured-image">
             <?php the_post_thumbnail('thumbnail'); ?>
           </div>
 
-          <h3><?php the_title(); ?></h3>
+          <p>Category: <?php the_category(); ?></p>
 
-          <p class="post-info"><?php echo "Published " . get_the_date(); echo " <br /> "; ?></p>
-        
           <?php the_excerpt(); ?>
+
+          <?php
+            $archive_year = get_the_time('Y');
+            $archive_month = get_the_time('m');
+            $archive_day = get_the_time('d');
+          ?>
+
+          <p class="post-info">Published <a href="<?php echo get_day_link($archive_year, $archive_month, $archive_day); ?>"><?php echo get_the_date(); ?></a></p>
 
           <a class="btn btn-primary" href="<?php the_permalink(); ?>">Read More</a>
         </div>
